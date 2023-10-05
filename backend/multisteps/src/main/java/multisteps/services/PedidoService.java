f4b1dac9566464678fd38cb341df22b34bcf661e
yuri neves <yurineves1934@gmail.com>
2023-10-05 17:48:41 -0300

    feat: create service pedido

---
+++ backend/multisteps/src/main/java/multisteps/services/PedidoService.java
@@ -0,0 +1,26 @@
+package multisteps.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import multisteps.entities.Pedido;
+import multisteps.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+  
+  @Autowired
+  private PedidoRepository pedidoRepository;
+
+  public void salvar(Pedido pedido) {
+    pedidoRepository.save(pedido);
+  }
+
+  public List<Pedido> findAll() {
+    List<Pedido> pedidos = pedidoRepository.findAll();
+
+    return pedidos;
+  }
+}
