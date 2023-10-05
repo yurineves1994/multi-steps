53b94254aa9cc8bac7e4a28388adae150520300c
yuri neves <yurineves1934@gmail.com>
2023-10-04 23:32:39 -0300

    feat: create masks project

---
+++ frontend/src/masks/masks.ts
@@ -0,0 +1,29 @@
+export const normalizePhoneNumber = (value: string | undefined) => {
+  if (!value) return '';
+
+  return value
+    .replace(/[\D]/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{5})(\d)/, '$1-$2')
+    .replace(/(-\d{4})(\d+?)/, '$1');
+};
+
+export const normalizeCnpjNumber = (value: string | undefined) => {
+  if (!value) return '';
+
+  return value
+    .replace(/[\D]/g, '')
+    .replace(/(\d{2})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1/$2')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1');
+};
+
+export const normalizeCepNumber = (value: string | undefined) => {
+  if (!value) return '';
+  return value
+    .replace(/\D/g, '')
+    .replace(/^(\d{5})(\d{3})+?$/, '$1-$2')
+    .replace(/(-\d{3})(\d+?)/, '$1');
+};
